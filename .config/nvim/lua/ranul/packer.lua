6827cbfea164466b585837d51683f4d77d89ebaf
Ranul Pallemulle <ranulp96@gmail.com>
2024-12-22 16:05:44 +0100

    tex

--- .config/nvim/lua/ranul/packer.lua
+++ .config/nvim/lua/ranul/packer.lua
@@ -41,4 +41,12 @@ return require('packer').startup(function(use)
   }
   use('lukas-reineke/indent-blankline.nvim')
   use('mhartington/formatter.nvim')
+  use('lervag/vimtex')
+  use({
+    "L3MON4D3/LuaSnip",
+  	-- follow latest release.
+  	tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  	-- install jsregexp (optional!:).
+  	run = "make install_jsregexp"
+  })
 end)
