cbab6a39397fa50b5a3fa02a5bc97930dea247ab
Ranul Pallemulle <ranulp96@gmail.com>
2026-03-31 21:50:28 +0200

    Remove cmp, luasnip

--- .config/nvim/lua/ranul/lazy.lua
+++ .config/nvim/lua/ranul/lazy.lua
@@ -45,20 +45,7 @@ local plugins = {
     'mbbill/undotree',
     'tpope/vim-fugitive',
     -- 'tpope/vim-sleuth',
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            -- LSP Support
-            {'williamboman/mason.nvim'}, -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-            {'neovim/nvim-lspconfig'},             -- Required
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-        }
-    },
+    'neovim/nvim-lspconfig',
     'lukas-reineke/indent-blankline.nvim',
     'mhartington/formatter.nvim',
     'lervag/vimtex',
