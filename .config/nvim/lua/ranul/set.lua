866381604f8831c8ce0bb8fd66fe47809692a76b
Ranul Pallemulle <ranulp96@gmail.com>
2026-04-06 08:47:27 +0200

    Preferences updates

--- .config/nvim/lua/ranul/set.lua
+++ .config/nvim/lua/ranul/set.lua
@@ -28,6 +28,8 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 -- vim.opt.colorcolumn = "80"
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "+1"
 
 vim.diagnostic.config({
     underline = true,
